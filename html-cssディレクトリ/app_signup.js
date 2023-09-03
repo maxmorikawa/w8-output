18c45a1c9e0e368b11ab722a9ffb86a00fc5a941
MASAYA MORIKAWA <mori2@tkf.att.ne.jp>
2023-09-03 20:18:14 +0900

    sign inページとsign upページを削除しました

--- html-cssディレクトリ/app_signup.js
+++
@@ -1,7 +0,0 @@
-// 保存
-function save() {
-  var youremail = document.getElementById("email-signup").value;
-  console.log(youremail);
-  var yourpassword = document.getElementById("password-signup").value;
-  localStorage.setItem(youremail, yourpassword);
-}
